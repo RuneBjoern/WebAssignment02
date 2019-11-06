dab6a632fa6f45fd72e56225fbec8015987c5340
Tobias Saaby Steffensen <tobias.saaby.steffensen@post.au.dk>
2019-11-06 15:52:04 +0100

    Rydder op

--- TrainingApp/src/app/app-routing.module.ts
+++ TrainingApp/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-=======
 import { WorkoutComponent } from './workout/workout.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ExerciseComponent } from './exercise/exercise.component';
->>>>>>> 4bb7f68f242628ad127212461982829d2c9247cc
 
 
 const routes: Routes = [
